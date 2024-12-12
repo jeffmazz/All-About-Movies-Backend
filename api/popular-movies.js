3b11e8562f2bef8dff557a290a9cf3fbd7363178
jeffmazz <jeff.mazzotta@hotmail.com>
2024-12-11 21:11:40 -0300

    Feat: criando função para filtrar array e rota popular-movies

---
+++ api/popular-movies.js
@@ -0,0 +1,26 @@
+const apiKey = process.env.API_KEY
+const options = {
+    method: "GET",
+    headers: {
+        accept : "application/json",
+        Authorization: `Bearer ${apiKey}`
+    }
+}
+
+import filter from "./fetch-data"
+
+module.exports = async(req, res) => {
+
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        if(!response.ok) throw new Error('Faliled to fetch popular movies from API')
+        const data = await response.json()
+        const results = data.results
+        const filteredResults = await filter(results)
+        return res.status(200).json(filteredResults)
+    } catch(err) {
+        console.error(err)
+        return res.status(500).json({error: err.message})
+    }
+
+}
