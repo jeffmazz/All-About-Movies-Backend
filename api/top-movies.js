a1473dc944934c3356ad063efd7e4362dcdee4ed
jeffmazz <jeff.mazzotta@hotmail.com>
2024-12-11 21:07:19 -0300

    Feat: criando função para filtrar array e rota popular-movies

--- api/top-movies.js
+++ api/top-movies.js
@@ -14,7 +14,28 @@ module.exports = async(req, res) => {
         if(!response.ok) throw new Error('Failed to fetch top rated movies from API')
         const data = await response.json()
         const results = data.results
-        return res.status(200).json(results)
+
+        const filteredResults = results.map(item => ({
+            id: item?.id,
+            backdrop_path: item?.backdrop_path,
+            title: item?.title,
+            original_title: item?.original_title,
+            overview: item?.overview,
+            vote_average: item?.vote_average,
+            vote_count: item?.vote_count,
+            release_date: item?.release_date,
+            popularity: item?.popularity,
+            first_air_date: item?.first_air_date,
+            known_for: item?.known_for.map(item => ({
+                id: item.id,
+                name: item.name,
+                title: item.title,
+                original_title: item.original_title,
+                vote_average: item.vote_average,
+            }))
+        }))
+
+        return res.status(200).json(filteredResults)
     } catch(err) {
         console.error(err)
         return res.status(500).json({error: err.message})
