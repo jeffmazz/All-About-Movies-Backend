fd7a2ada24064332a52d3ab5f529b1792b4a7c22
jeffmazz <jeff.mazzotta@hotmail.com>
2024-12-12 21:58:25 -0300

    .

--- api/searched.js
+++ api/searched.js
@@ -2,7 +2,7 @@ import {fetchData} from './fetch-data'
 
 module.exports = async(req, res) => {
     const {q} = req.query
-    if(!query) return res.status(400).json({error: "Query not provided"})
+    if(!q) return res.status(400).json({error: "Query not provided"})
     const url = `https://api.themoviedb.org/3/search/multi?query=${q}&include_adult=false&language=en-US&page=1`
     fetchData(url, req, res)
 }
