77d504aa1411b188bf96eb5279b5690cc2485bfa
jeffmazz <jeff.mazzotta@hotmail.com>
2024-12-14 10:11:40 -0300

    Fix: corrigindo paginação na rota top-movies.js

--- api/top-movies.js
+++ api/top-movies.js
@@ -1,7 +1,7 @@
 import {fetchData} from "./fetch-data"
 
 module.exports = async(req, res) => {
-    const {pageNumber} = req.query || 1
+    const {pageNumber} = req.query ?? 1
     const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pageNumber}`
     fetchData(url, req, res)
 }
